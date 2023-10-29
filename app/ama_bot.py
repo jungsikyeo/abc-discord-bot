6720d73f4f7107e4d5475760177262f2bd6787f1
jungsik.yeo <mdotcom12@naver.com>
2023-10-30 01:41:56 +0900

    [fix] ama role bulk fix

--- app/ama_bot.py
+++ app/ama_bot.py
@@ -166,9 +166,6 @@ async def end_ama(ctx):
         capture_loop.cancel()
         await capture_final_snapshot(ctx)
 
-        members_to_assign_role = [ctx.guild.get_member(member_id) for member_id in message_counts.keys()]
-        await assign_roles(ctx, ama_role_id, members_to_assign_role)
-
         role_name = discord.utils.get(ctx.guild.roles, id=ama_role_id).name
         await create_and_upload_excel(ctx, snapshots, role_name)
 
@@ -608,364 +605,110 @@ async def get_user_summary_from_db(role_name, user_id):
 
 
 @bot.command()
-async def bulk_assign_role(ctx, role: discord.Role):
-    user_ids = [
-        1000693846205800469,
-        1012082539566989342,
-        1013591786985365575,
-        1015737051469062245,
-        1018859325227278416,
-        1021180252279554121,
-        1022958295482388481,
-        1032222097646288906,
-        1034103137449029652,
-        1034489951309545602,
-        1035899540429086751,
-        1039500529816047688,
-        1043636207516667965,
-        1045113473073430598,
-        1056491548721819688,
-        1061562005187727473,
-        1065936887111503903,
-        1065954166754787398,
-        1066080002845708440,
-        1068111322811727943,
-        1068922997651542046,
-        1070037529799168122,
-        1078943468983943189,
-        1084152932762669187,
-        1085315720365224026,
-        1087766296390807552,
-        1090764482990592010,
-        1091460777715708094,
-        1092849329716351138,
-        1093822671189442570,
-        1114320486340513822,
-        1114984309758099579,
-        1120863717320568902,
-        1133718478499627129,
-        1142562643505336380,
-        1143459001368064081,
-        1146122212437393458,
-        1149087935216504973,
-        1152671542975664200,
-        1154434984220827739,
-        1155825545007857694,
-        1157204746432696354,
-        1166684499334148168,
-        190868525873758209,
-        194828121499893771,
-        216648801899905024,
-        228186428205432833,
-        236438774861660160,
-        244381062753419264,
-        285167032885051392,
-        307546180169105409,
-        326352735018680321,
-        332183935364759562,
-        336544647272857600,
-        341686158180483074,
-        342294904245846016,
-        346852297697132544,
-        351236056483364864,
-        363285769756082178,
-        364256788918304768,
-        365899136198901771,
-        392304188274769941,
-        394784215981359115,
-        395814118461800449,
-        404928667425308682,
-        410467315608584192,
-        426382192634232833,
-        428964034159575040,
-        433564409051217920,
-        452298540509560843,
-        456790953563258882,
-        464797945573933066,
-        466568490669834255,
-        469481016093048854,
-        482287486018781225,
-        499958513217568768,
-        502855264051920897,
-        507341260650971137,
-        507561287748943882,
-        516820278870016035,
-        516825481257943041,
-        518723907813900305,
-        518798517003878400,
-        521301715199328257,
-        531781653954035715,
-        534374899117064204,
-        545847242632855573,
-        546708520343699466,
-        605703134098227231,
-        609934212014932042,
-        627498599458144257,
-        628074315274911744,
-        638479745906114572,
-        693416975220604949,
-        700083885353992794,
-        705055653437112410,
-        710488978020761612,
-        713701674291822652,
-        717266594673328198,
-        717685615747137576,
-        730074607377449013,
-        748186107539226664,
-        751432663147085825,
-        761610131154403349,
-        761763558404128779,
-        764739699834093580,
-        764842596421074954,
-        770170022223020054,
-        802951857898913824,
-        804621142543171614,
-        806235134516002897,
-        809278064018194442,
-        812201142041837600,
-        817751298119827499,
-        827555957617590303,
-        831157309367517284,
-        831797383468810241,
-        831856737606565928,
-        835123641334628392,
-        836658587534229504,
-        837972644409114634,
-        845309743518056498,
-        845706794080403496,
-        847568725535621140,
-        849329641927737384,
-        850340759303487498,
-        850998633649274930,
-        855422311548977172,
-        857200472120229889,
-        858153167903653908,
-        859215503481503784,
-        865217125161500704,
-        867852104084357142,
-        869547323385065523,
-        878530522563887104,
-        881548267144482866,
-        883764527332737064,
-        885034104947605545,
-        885498997396107355,
-        886189458486083596,
-        886373719289778226,
-        888327065521258508,
-        889190633036714084,
-        889535587793662043,
-        890160708891856956,
-        890223882039066644,
-        891672724043350096,
-        891695358881718313,
-        892249831861534720,
-        893131966642270298,
-        894080673407717416,
-        896281310925041694,
-        896451419350134824,
-        900580770362576966,
-        902003287157518367,
-        902608853999427604,
-        903417556990296085,
-        905728632914317352,
-        907648295793487872,
-        908380522403750008,
-        909080516832141312,
-        913223759400415272,
-        913462969537531944,
-        913694863847993354,
-        914878793854885908,
-        914940680713936927,
-        915139373098500117,
-        917643567776821278,
-        917983330719371344,
-        918264789694812160,
-        918277794407018506,
-        921156882968760351,
-        921711266165313546,
-        923569969411792957,
-        924627466134372413,
-        925486418883665960,
-        926163499435040769,
-        926351929091719189,
-        926741066156236850,
-        927229076081610762,
-        927380365612572732,
-        928677684022759444,
-        929409454838005770,
-        930730752058994719,
-        931076583396102184,
-        931230981086670849,
-        931857748486942810,
-        934146025311051817,
-        936371257413345300,
-        937767058337329192,
-        938802213009117214,
-        939467740400472098,
-        940788149691486248,
-        941010057406079046,
-        942027723130437633,
-        942319099470553120,
-        942871479996010576,
-        944555712695115786,
-        945495792523829310,
-        947978829819957358,
-        948285725915369482,
-        948529986858520617,
-        949271565810425896,
-        950735073378988082,
-        950763933038440479,
-        951420547584110613,
-        952224070546624582,
-        952249074705371177,
-        952263511348760647,
-        952277050041978911,
-        952279976344948846,
-        952399346303893524,
-        952500596395614218,
-        952503065917935676,
-        952503805256278056,
-        952532222508933130,
-        952532347142680599,
-        952534057269469245,
-        952562781385805864,
-        952576465726226432,
-        952586395191500852,
-        952597610093486090,
-        952598691624124496,
-        952604917472845845,
-        952612912713854996,
-        952626002331312158,
-        952679345762213978,
-        952681914991513690,
-        952746156495085608,
-        952781661639164004,
-        952792958694993941,
-        952815496036765737,
-        952821785433358386,
-        953247410363183145,
-        953600347925020682,
-        954682271540076554,
-        954866675814121583,
-        956211462626480199,
-        956237355965100082,
-        960573814851375184,
-        961044323170918471,
-        961547604553007104,
-        963115711553765488,
-        964907903515492475,
-        965058320891248650,
-        965262102820429834,
-        965597801222381638,
-        965866307436286015,
-        966005580827353278,
-        966749868708220978,
-        971090377354399774,
-        972240396740599888,
-        974019048490815620,
-        979889653916262420,
-        981305139249954897,
-        985223338383273994,
-        988439692682862622,
-        992377835815706624,
-        993920014480580749,
-        994508399641378897,
-        995954911793647687,
-        996166536454742067,
-        997417987084664864,
-        998121253112127578,
-        999606976180924427
-    ]
+async def bulk_assign_role(ctx, role: Union[discord.Role, int, str]):
+    # 입력값이 롤 객체인 경우
+    if isinstance(role, discord.Role):
+        role_found = role
+    # 입력값이 역할 ID인 경우
+    elif isinstance(role, int):
+        role_found = discord.utils.get(ctx.guild.roles, id=role)
+    # 입력값이 역할 이름인 경우
+    else:
+        role_found = discord.utils.get(ctx.guild.roles, name=role)
 
-    for user_id in user_ids:
-        member = ctx.guild.get_member(user_id)
-        if member is not None:
-            try:
-                await member.add_roles(role)
-                await ctx.send(f"Role {role.name} has been assigned to {member.display_name}.")
-            except discord.Forbidden:
-                await ctx.send(f"Failed to assign role to {member.display_name}. Check the bot's permissions.")
-            except discord.HTTPException as e:
-                await ctx.send(f"HTTP exception while assigning role to {member.display_name}: {str(e)}")
-        else:
-            await ctx.send(f"Member with ID {user_id} not found.")
+    if role_found is None:
+        embed = Embed(title="Error",
+                      description=f"❌ Role not found for name, ID, or mention {role}. Please enter a valid role name, ID, or mention.\n\n"
+                                  f"❌ {role} 이름, ID 또는 멘션의 역할을 찾을 수 없습니다. 올바른 역할 이름, ID 또는 멘션을 입력해주세요.",
+                      color=0xff0000)
+        await ctx.reply(embed=embed, mention_author=True)
+        return
+
+    connection = db.get_connection()
+    cursor = connection.cursor()
+    user_ids = []
+    try:
+        cursor.execute("""
+            select user_id
+            from ama_users_summary
+            where role_name = %s
+            and valid_messages > 0
+            limit 10
+            """, role_found.name)
+        result = cursor.fetchall()
+        if result:
+            user_ids.append(result['user_id'])
+        if len(user_ids) > 0:
+            for user_id in user_ids:
+                member = ctx.guild.get_member(user_id)
+                if member is not None:
+                    try:
+                        await member.add_roles(role_found)
+                        await ctx.send(f"Role {role_found.name} has been assigned to {member.display_name}.")
+                    except discord.Forbidden:
+                        await ctx.send(f"Failed to assign role to {member.display_name}. Check the bot's permissions.")
+                    except discord.HTTPException as e:
+                        await ctx.send(f"HTTP exception while assigning role to {member.display_name}: {str(e)}")
+                else:
+                    await ctx.send(f"Member with ID {user_id} not found.")
+    except Exception as e:
+        logger.error(f'DB error: {e}')
+    finally:
+        cursor.close()
+        connection.close()
 
 
 @bot.command()
-async def bulk_remove_role(ctx, role: discord.Role):
-    user_ids = [
-        332183935364759562,
-        770170022223020054,
-        952821785433358386,
-        952399346303893524,
-        865217125161500704,
-        893131966642270298,
-        1039500529816047688,
-        952263511348760647,
-        952597610093486090,
-        952500596395614218,
-        952576465726226432,
-        952746156495085608,
-        428964034159575040,
-        952532222508933130,
-        952612912713854996,
-        952532347142680599,
-        952562781385805864,
-        952792958694993941,
-        952781661639164004,
-        952604917472845845,
-        952681914991513690,
-        507561287748943882,
-        902608853999427604,
-        456790953563258882,
-        952534057269469245,
-        952249074705371177,
-        952279976344948846,
-        952626002331312158,
-        952679345762213978,
-        952224070546624582,
-        952277050041978911,
-        952586395191500852,
-        952503805256278056,
-        952815496036765737,
-        952503065917935676,
-        952598691624124496,
-        1012082539566989342,
-        888327065521258508,
-        751432663147085825,
-        902003287157518367,
-        948529986858520617,
-        998121253112127578,
-        831157309367517284,
-        931857748486942810,
-        927229076081610762,
-        954682271540076554,
-        1154434984220827739,
-        857200472120229889,
-        638479745906114572,
-        518723907813900305,
-        926741066156236850,
-        950763933038440479,
-        717266594673328198,
-        849329641927737384,
-        924627466134372413,
-        464797945573933066,
-        827555957617590303,
-        341686158180483074
-    ]
+async def bulk_remove_role(ctx, role: Union[discord.Role, int, str]):
+    # 입력값이 롤 객체인 경우
+    if isinstance(role, discord.Role):
+        role_found = role
+    # 입력값이 역할 ID인 경우
+    elif isinstance(role, int):
+        role_found = discord.utils.get(ctx.guild.roles, id=role)
+    # 입력값이 역할 이름인 경우
+    else:
+        role_found = discord.utils.get(ctx.guild.roles, name=role)
 
-    for user_id in user_ids:
-        member = ctx.guild.get_member(user_id)
-        if member is not None:
-            try:
-                await member.remove_roles(role)
-                await ctx.send(f"Role {role.name} has been removed from {member.display_name}.")
-            except discord.Forbidden:
-                await ctx.send(f"Failed to remove role from {member.display_name}. Check the bot's permissions.")
-            except discord.HTTPException as e:
-                await ctx.send(f"HTTP exception while removing role from {member.display_name}: {str(e)}")
-        else:
-            await ctx.send(f"Member with ID {user_id} not found.")
+    if role_found is None:
+        embed = Embed(title="Error",
+                      description=f"❌ Role not found for name, ID, or mention {role}. Please enter a valid role name, ID, or mention.\n\n"
+                                  f"❌ {role} 이름, ID 또는 멘션의 역할을 찾을 수 없습니다. 올바른 역할 이름, ID 또는 멘션을 입력해주세요.",
+                      color=0xff0000)
+        await ctx.reply(embed=embed, mention_author=True)
+        return
+
+    connection = db.get_connection()
+    cursor = connection.cursor()
+    user_ids = []
+    try:
+        cursor.execute("""
+            select user_id
+            from ama_users_summary
+            where role_name = %s
+            and valid_messages = 0
+            """, role_found.name)
+        result = cursor.fetchall()
+        if result:
+            user_ids.append(result['user_id'])
+        if len(user_ids) > 0:
+            for user_id in user_ids:
+                member = ctx.guild.get_member(user_id)
+                if member is not None:
+                    try:
+                        await member.remove_roles(role_found)
+                        await ctx.send(f"Role {role_found.name} has been removed from {member.display_name}.")
+                    except discord.Forbidden:
+                        await ctx.send(f"Failed to remove role from {member.display_name}. Check the bot's permissions.")
+                    except discord.HTTPException as e:
+                        await ctx.send(f"HTTP exception while removing role from {member.display_name}: {str(e)}")
+                else:
+                    await ctx.send(f"Member with ID {user_id} not found.")
+    except Exception as e:
+        logger.error(f'DB error: {e}')
+    finally:
+        cursor.close()
+        connection.close()
 
 
 bot.run(bot_token)

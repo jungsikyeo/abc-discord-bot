69a9a49425b767f7de8ed8138ba4099318b875df
jungsik.yeo <mdotcom12@naver.com>
2023-11-04 08:45:03 +0900

    [fix] coin value fix

--- app/alphabot2.py
+++ app/alphabot2.py
@@ -2208,7 +2208,7 @@ async def coin(ctx, coin_symbol: str, period: str = "1day"):
     embed = discord.Embed(title=f"{coin_name}", description=f"{coin_name} {period_str} Chart Based on Binance",
                           color=0xEFB90A)
     embed.add_field(name="24h Change",
-                    value=f"```diff\n{change_prefix}{change_24h:,.2f} ({change_prefix}{change_24h_percent}%)```")
+                    value=f"```diff\n{change_prefix}{change_24h:,.4f} ({change_prefix}{change_24h_percent}%)```")
     embed.add_field(name="24h High", value=f"```{high_24h:,.2f}```")
     embed.add_field(name="24h Low", value=f"```{low_24h:,.2f}```")
     embed.add_field(name=f"24h Volume ({base_coin})", value=f"```{volume_24h_volume:,.2f}```")

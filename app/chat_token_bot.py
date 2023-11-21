15b5019af71c0bd9c7e5914ad2f64b704f4d0c1c
jungsik.yeo <mdotcom12@naver.com>
2023-11-21 15:52:55 +0900

    [fix] c2e fix

--- app/chat_token_bot.py
+++ app/chat_token_bot.py
@@ -6,15 +6,15 @@ import datetime
 import random
 import asyncio
 import pytz
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
 from dotenv import load_dotenv
 from pymysql.cursors import DictCursor
 from dbutils.pooled_db import PooledDB
 from discord.ext import commands
 from discord import Embed, Button, ButtonStyle
-from discord.ui import View
+from discord.ui import View, button, Modal, InputText
+from discord.interactions import Interaction
 from datetime import datetime, timedelta
+
 
 load_dotenv()
 
@@ -26,7 +26,6 @@ mysql_port = os.getenv("MYSQL_PORT")
 mysql_id = os.getenv("MYSQL_ID")
 mysql_passwd = os.getenv("MYSQL_PASSWD")
 mysql_db = os.getenv("MYSQL_DB")
-
 
 logging.basicConfig(
     level=logging.INFO,
@@ -63,20 +62,107 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=f"{command_flag}", intents=intents)
 db = Database(mysql_ip, mysql_port, mysql_id, mysql_passwd, mysql_db)
 
-
 tokens_data = {}
 winner_users = {}
 used_verify_lm = []
 weekly_top = []
 
-searchfi_amount = 110
-lm_amount = 165
+searchfi_amount = 100
 min_win = 1
 max_win = 5
 win_limit = 4
 
+lock_status = True
+
 exclude_role_list = list(map(int, os.getenv('C2E_EXCLUDE_ROLE_LIST').split(',')))
 enabled_channel_list = list(map(int, os.getenv('C2E_ENABLED_CHANNEL_LIST').split(',')))
+
+
+class TokenSettingsModal(Modal):
+    def __init__(self, data):
+        super().__init__(title="SF Token Settings")
+        self.add_item(InputText(label="Daily Token Limit",
+                                placeholder="Enter the daily token limit",
+                                value=f"{data.get('daily_token_limit')}"))
+        self.add_item(InputText(label="Minimum Tokens per Win",
+                                placeholder="Enter the minimum tokens per win",
+                                value=f"{data.get('min_win')}"))
+        self.add_item(InputText(label="Maximum Tokens per Win",
+                                placeholder="Enter the maximum tokens per win",
+                                value=f"{data.get('max_win')}"))
+        self.add_item(InputText(label="Win Limit per User",
+                                placeholder="Enter the win limit per user",
+                                value=f"{data.get('win_limit')}"))
+
+    async def callback(self, interaction: Interaction):
+        # 데이터베이스 업데이트 로직
+        daily_limit = self.children[0].value
+        min_tokens = self.children[1].value
+        max_tokens = self.children[2].value
+        user_limit = self.children[3].value
+
+        connection = db.get_connection()
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+                UPDATE c2e_token_tracking 
+                SET 
+                    daily_token_limit = %s,
+                    min_win = %s,
+                    max_win = %s,
+                    win_limit = %s
+                WHERE type = %s
+            """, (daily_limit, min_tokens, max_tokens, user_limit, "searchfi"))
+            connection.commit()
+        except Exception as e:
+            connection.rollback()
+            logger.error(f'TokenSettingsModal db error: {e}')
+        finally:
+            cursor.close()
+            connection.close()
+
+        await interaction.response.send_message("SF 토큰 설정이 업데이트되었습니다!\n\n"
+                                                "SF Token settings updated successfully!", ephemeral=True)
+
+
+# 버튼 클래스 정의
+class TokenSettingsButton(View):
+    def __init__(self, db):
+        super().__init__()
+        self.db = db
+
+    @button(label="setting", style=discord.ButtonStyle.primary, custom_id="setting_sftoken_button")
+    async def button_add_setting(self, _, interaction: Interaction):
+        searchfi_data = {}
+        connection = db.get_connection()
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+                SELECT 
+                    daily_token_limit,
+                    min_win,
+                    max_win,
+                    win_limit
+                FROM c2e_token_tracking
+                WHERE type = %s
+            """, "searchfi")
+            searchfi_data = cursor.fetchone()
+        except Exception as e:
+            connection.rollback()
+            logger.error(f'button_add_setting db error: {e}')
+        finally:
+            cursor.close()
+            connection.close()
+        await interaction.response.send_modal(modal=TokenSettingsModal(searchfi_data))
+
+
+@bot.command()
+@commands.has_any_role('SF.Team', 'SF.Guardian', 'SF.dev')
+async def setting_sf_tokens(ctx):
+    embed = Embed(title="SF Token Settings", description="아래 버튼으로 SF 토큰을 세팅해주세요.\n\n"
+                                                 "Please setting SF Token using the button below.", color=0xFFFFFF)
+    view = TokenSettingsButton(db)
+    await ctx.reply(embed=embed, view=view, mention_author=True)
 
 
 # 한국 시간대 기준 정오(낮 12시) 시간 구하기
@@ -92,26 +178,6 @@ def random_time(base, max_duration):
     return base + random.randint(0, max_duration)
 
 
-async def weekly_top_reset():
-    # 1주일(604800초) 대기
-    await asyncio.sleep(604800)
-
-    # 현재 시간을 기준으로 새로운 주간 탑 상품 시간 설정
-    now = datetime.now().timestamp()
-    global weekly_top
-    weekly_top = [
-        random_time(now, 204800),
-        random_time(now + 204800, 200000),
-        random_time(now + 404800, 200000)
-    ]
-
-    # 로그 기록
-    logger.info(f"Weekly top reset at {datetime.now()}. Next times: {weekly_top}")
-
-    # 함수를 다시 비동기적으로 호출하여 주기적으로 재설정 계속
-    asyncio.create_task(weekly_top_reset())
-
-
 @bot.event
 async def on_ready():
     # 봇이 준비되었을 때 실행할 코드
@@ -122,29 +188,133 @@ async def on_ready():
     cursor = connection.cursor()
 
     try:
-        # SEARCHFI 토큰 초기화 및 스케줄링
-        next_reset = get_noon_kst() + 1000
         cursor.execute("""
-            INSERT INTO c2e_token_tracking (type, reset_at, still_available) 
-            VALUES (%s, %s, %s) 
-            ON DUPLICATE KEY UPDATE reset_at = VALUES(reset_at)
-        """, ("searchfi", next_reset, searchfi_amount))
+            SELECT reset_at, still_available, daily_token_limit, min_win, max_win, win_limit
+            FROM c2e_token_tracking WHERE type = %s
+        """, ("searchfi",))
+        searchfi_data = cursor.fetchone()
+
+        global searchfi_amount, min_win, max_win, win_limit, lock_status
+
+        # SEARCHFI 토큰 초기화 및 스케줄링
+        if not searchfi_data:
+            next_reset = get_noon_kst()
+            cursor.execute("""
+                INSERT INTO c2e_token_tracking (type, reset_at) 
+                VALUES (%s, %s) 
+            """, ("searchfi", next_reset))
+
+            cursor.execute("""
+                SELECT reset_at, still_available, daily_token_limit, min_win, max_win, win_limit
+                FROM c2e_token_tracking WHERE type = %s
+            """, ("searchfi",))
+            searchfi_data = cursor.fetchone()
+
+            searchfi_amount = searchfi_data['still_available']
+            min_win = searchfi_data['min_win']
+            max_win = searchfi_data['max_win']
+            win_limit = searchfi_data['win_limit']
 
         # 커밋
         connection.commit()
 
-        cursor.execute("""
-            SELECT reset_at, still_available FROM c2e_token_tracking WHERE type = %s
-        """, ("searchfi",))
-        searchfi_data = cursor.fetchone()
-
-        print(f"searchfi {datetime.fromtimestamp(searchfi_data['reset_at'])}, {searchfi_data['still_available']}")
+        print(
+            f"searchfi ready: {datetime.fromtimestamp(searchfi_data['reset_at'])}, {searchfi_data['still_available']}")
 
         asyncio.create_task(schedule_reset("searchfi", False))
+
+        lock_status = False
 
     except Exception as e:
         logger.error(f'Error in on_ready: {e}')
         connection.rollback()
+    finally:
+        cursor.close()
+        connection.close()
+
+
+async def schedule_reset(token_type, run_type=True):
+    # 토큰 정보 가져오기
+    connection = db.get_connection()
+    cursor = connection.cursor()
+    try:
+        global searchfi_amount, min_win, max_win, win_limit
+        cursor.execute("""
+                SELECT reset_at, still_available, daily_token_limit, min_win, max_win, win_limit
+                FROM c2e_token_tracking WHERE type = %s
+            """, ("searchfi",))
+        searchfi_data = cursor.fetchone()
+
+        searchfi_amount = searchfi_data['daily_token_limit']
+        min_win = searchfi_data['min_win']
+        max_win = searchfi_data['max_win']
+        win_limit = searchfi_data['win_limit']
+
+        if run_type:
+            next_reset = int(searchfi_data['reset_at']) + 43200  # 다음 리셋 시간 계산
+
+            # 데이터베이스에서 토큰 리셋 시간 업데이트
+            cursor.execute("""
+                UPDATE c2e_token_tracking SET reset_at = %s, still_available = %s 
+                WHERE type = %s
+            """, (next_reset, searchfi_amount, token_type))
+
+            connection.commit()
+        else:
+            next_reset = int(searchfi_data['reset_at'])
+
+        # 토큰 데이터 리셋
+        global winner_users, tokens_data
+        tokens_data[token_type] = None
+        winner_users = {}
+
+        await schedule_give(token_type)
+
+        # 다음 리셋까지 대기
+        print(f"sleep: {next_reset - datetime.now().timestamp()}")
+        await asyncio.sleep(next_reset - datetime.now().timestamp())
+
+        # 다음 리셋 스케줄링
+        print(f"resetting tokens at, {datetime.fromtimestamp(next_reset)}, {token_type}")
+    except Exception as e:
+        connection.rollback()
+        logger.error(f'schedule_reset db error: {e}')
+    finally:
+        cursor.close()
+        connection.close()
+        asyncio.create_task(schedule_reset(token_type))
+
+
+async def schedule_give(token_type):
+    # 토큰 정보 가져오기
+    connection = db.get_connection()
+    cursor = connection.cursor()
+    try:
+        # 남은 토큰 수량 확인
+        cursor.execute("""
+            SELECT still_available FROM c2e_token_tracking WHERE type = %s
+        """, (token_type,))
+        available = cursor.fetchone()['still_available']
+
+        # 남은 시간 계산 (예: 자정까지 남은 시간)
+        now = datetime.now()
+        midnight = datetime(now.year, now.month, now.day) + timedelta(days=1)
+        remaining_seconds = (midnight - now).total_seconds()
+
+        # 새로운 토큰 지급 주기 계산
+        if available > 0:
+            new_rate = remaining_seconds / available
+            # 랜덤 시간 추가: -3분 ~ +3분
+            random_offset = random.randint(-180, 180)  # 초 단위로 -180초 ~ 180초
+            next_give_time = now.timestamp() + new_rate + random_offset
+        else:
+            next_give_time = midnight.timestamp()  # 토큰이 없으면 다음 날로 설정
+
+        # 토큰 지급 시간 업데이트
+        tokens_data[token_type] = next_give_time
+    except Exception as e:
+        connection.rollback()
+        logger.error(f'schedule_give db error: {e}')
     finally:
         cursor.close()
         connection.close()
@@ -166,16 +336,17 @@ async def on_message(message):
 
     # tokensData와 winnerUsers를 확인하여 토큰 지급 여부 결정
     type1 = "searchfi"
-    global winner_users, tokens_data
+    global winner_users, tokens_data, lock_status
     current_timestamp = datetime.now().timestamp()
     if tokens_data.get(type1):
         print(datetime.fromtimestamp(current_timestamp), datetime.fromtimestamp(tokens_data[type1]))
-    if tokens_data.get(type1) and current_timestamp > tokens_data[type1]:
+    if not lock_status and tokens_data.get(type1) and current_timestamp > tokens_data[type1]:
         if not winner_users.get(message.author.id) or winner_users[message.author.id] < win_limit:
+            lock_status = True
             # searchfi 토큰 지급
             await give_points(message, type1)
-            winner_users[message.author.id] = winner_users.get(message.author.id, 0) + 1
-            tokens_data[type1] = None  # 토큰 지급 후 데이터 업데이트
+            await schedule_give(type1)
+            lock_status = False
 
     # 명령어 처리를 위해 기본 on_message 핸들러 호출
     await bot.process_commands(message)
@@ -208,6 +379,16 @@ async def give_points(message, token_type):
             ON DUPLICATE KEY UPDATE tokens = tokens + VALUES(tokens)
         """, (message.author.id, token_amount))
 
+        # 사용자 토큰 부여 로그
+        cursor.execute("""
+            INSERT INTO c2e_token_logs (
+                user_id, tokens, user_name, send_user_id, send_user_name, channel_id, channel_name, action_type
+            ) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, (message.author.id, token_amount, message.author.name,
+              'bot', 'bot', message.channel.id, message.channel.name, 'CHAT'))
+        print(f"{message.channel.name} -> {message.author.name} : {token_amount}")
+
         # 커밋
         connection.commit()
 
@@ -220,10 +401,11 @@ async def give_points(message, token_type):
             description=f"You just won **{token_amount}** test tokens!",
             color=0x9da1ef
         )
-        embed.set_image(url="https://cdn.discordapp.com/attachments/955428076651679846/1091499808960811008/IMG_0809.gif")
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/955428076651679846/1091499808960811008/IMG_0809.gif")
 
         # 메시지 전송
-        await message.channel.send(embed=embed)
+        await message.reply(embed=embed)
     except Exception as e:
         connection.rollback()
         logger.error(f'Error in give_points: {e}')
@@ -232,125 +414,12 @@ async def give_points(message, token_type):
         connection.close()
 
 
-# 암호화 키 설정
-key = bytes.fromhex(os.getenv('ENCRYPTION_KEY_HEX'))
-iv = bytes.fromhex(os.getenv('ENCRYPTION_IV_HEX'))
-
-
-# 데이터 암호화
-def encrypt_data(user_id, amount):
-    backend = default_backend()
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    encryptor = cipher.encryptor()
-    data = f'{user_id},{amount},{int(datetime.now().timestamp())}'
-    encrypted = encryptor.update(data.encode('utf-8')) + encryptor.finalize()
-    return encrypted.hex()
-
-
-# 데이터 복호화
-def decrypt_data(data):
-    backend = default_backend()
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    decryptor = cipher.decryptor()
-    decrypted = decryptor.update(bytes.fromhex(data)) + decryptor.finalize()
-    return dict(zip(['u', 't'], decrypted.decode('utf-8').split(',')))
-
-
-async def give_weekly_top_prize(message):
-    tokens = 100  # 예시 토큰 수량
-    encrypted_data = encrypt_data(message.author.id, tokens)
-
-    # 임베드 메시지 생성
-    embed = Embed(
-        title="Congratulations 🎉 🎉",
-        description=f"You just won **{tokens}** LM tokens! Submit your wallet now to receive your prize",
-        color=0x9da1ef
-    )
-    embed.set_image(url="https://cdn.discordapp.com/attachments/955428076651679846/1091499809317335212/IMG_0810.gif")
-
-    # 버튼 컴포넌트 생성
-    button = Button(label="Submit Wallet", style=ButtonStyle.primary, custom_id=f'gw-{encrypted_data}')
-    view = View()
-    view.add_item(button)
-
-    # 메시지와 함께 임베드와 버튼 전송
-    await message.channel.send(embed=embed, view=view)
-
-
-async def schedule_reset(token_type, light):
-    # 토큰 정보 가져오기
-    connection = db.get_connection()
-    cursor = connection.cursor()
-    try:
-        cursor.execute("""
-            SELECT reset_at, still_available FROM c2e_token_tracking WHERE type = %s
-        """, token_type)
-        searchfi_data = cursor.fetchone()
-        next_reset = int(searchfi_data['reset_at']) + 1000  # 다음 리셋 시간 계산
-
-        # 데이터베이스에서 토큰 리셋 시간 업데이트
-        cursor.execute("""
-            UPDATE c2e_token_tracking SET reset_at = %s, still_available = %s 
-            WHERE type = %s
-        """, (next_reset, searchfi_amount if token_type == "searchfi" else lm_amount, token_type))
-
-        connection.commit()
-
-        # 토큰 데이터 리셋
-        global winner_users, tokens_data
-        tokens_data[token_type] = None
-        winner_users = {}
-
-        await schedule_give(token_type)
-
-        # 다음 리셋까지 대기
-        # while True:
-        #     if (next_reset - datetime.now().timestamp()) > 0:
-        #         break
-        print(f"sleep: {next_reset - datetime.now().timestamp()}")
-        await asyncio.sleep(next_reset - datetime.now().timestamp())
-        # await asyncio.sleep(43200)
-
-        # 다음 리셋 스케줄링
-        print(f"resetting tokens at, {datetime.fromtimestamp(next_reset)}, {token_type}")
-        asyncio.create_task(schedule_reset(token_type, light))
-    except Exception as e:
-        connection.rollback()
-        logger.error(f'schedule_reset db error: {e}')
-    finally:
-        cursor.close()
-        connection.close()
-
-
-async def schedule_give(token_type):
-    # 토큰 정보 가져오기
-    connection = db.get_connection()
-    cursor = connection.cursor()
-    try:
-        cursor.execute("""
-            SELECT reset_at, still_available FROM c2e_token_tracking WHERE type = %s
-        """, token_type)
-        tok_info = cursor.fetchone()
-
-        logger.info(f"tokInfo scheduleGive {tok_info}, {token_type}")
-
-        if tok_info and tok_info['still_available'] > 0:
-            # 지급 시간 계산
-            rate = (tok_info['still_available'] / ((min_win + max_win) / 2)) + 1
-            give_points_in = round((int(tok_info['reset_at']) - datetime.now().timestamp()) / rate)
-            randomized = round((give_points_in * 0.80) + (random.random() * give_points_in * 0.40))
-
-            logger.info(f"Give points in {int(datetime.now().timestamp()) + (randomized / 1000)} for {token_type}")
-
-            global tokens_data
-            tokens_data[token_type] = int(datetime.now().timestamp()) + (randomized / 1000)
-
-    except Exception as e:
-        connection.rollback()
-        logger.error(f'schedule_give db error: {e}')
-    finally:
-        cursor.close()
-        connection.close()
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    else:
+        logger.error(f"An error occurred: {str(error)}")
 
 
 bot.run(bot_token)

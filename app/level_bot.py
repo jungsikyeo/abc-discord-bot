2149d4e65b4cc8c8631010ca5b944c7439c8a375
jungsik.yeo <mdotcom12@naver.com>
2024-04-02 20:24:56 +0900

    [fix] rank search delay fix

--- app/level_bot.py
+++ app/level_bot.py
@@ -312,12 +312,12 @@ async def get_rank(ctx: ApplicationContext,
     if rank_search_users and rank_search_users.get(user_id, None) and ctx.user.id not in no_rank_members:
         prev_time = rank_search_users.get(user_id, current_time)
         time_spent = current_time - prev_time
-        doing_time = datetime.fromtimestamp(prev_time + 60*60*8)    # 9시간 딜레이 세팅
+        doing_time = datetime.fromtimestamp(prev_time + 60*60*8)    # 8시간 딜레이 세팅
         doting_timestamp = int(doing_time.timestamp())
         if time_spent < 60*60*8:
             embed = make_embed({
                 "title": "Error",
-                "description": "Rank command inquiry is possible every nine hours.\n"
+                "description": "Rank command inquiry is possible every 8 hours.\n"
                                f"Your next command query time is <t:{doting_timestamp}>",
                 "color": 0xff0000,
             })

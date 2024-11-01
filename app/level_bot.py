01c2aeac801d9bbe7708a3b3fbd78a19ba034967
jungsik.yeo <mdotcom12@naver.com>
2024-11-01 23:53:16 +0900

    [fix] test

--- app/level_bot.py
+++ app/level_bot.py
@@ -858,7 +858,7 @@ async def give_role_top_users(ctx: ApplicationContext):
                     # 멤버가 상위 400명 밖이라면 역할 제거
                     if pioneer_role in member.roles:
                         await member.remove_roles(pioneer_role)
-                        logger.info(f"[{member_index}]{member.name} ({member.id}) -> Not in top 200, removed pioneer_role")
+                    logger.info(f"[{member_index}]{member.name} ({member.id}) -> Not in top 200, removed pioneer_role")
 
             embed = make_embed({
                 "title": "Top Users Refreshed!",
